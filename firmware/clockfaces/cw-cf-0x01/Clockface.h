a5c8ce919e1ca70ed3c25c0b08a6dfb99d97b2f7
kglx <kglx.bian@gmail.com>
2026-06-10 23:15:32 +0800

    git commit -m "Convert clockface submodules to regular directories"

---
+++ firmware/clockfaces/cw-cf-0x01/Clockface.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include "gfx/Super_Mario_Bros__24pt7b.h"
+
+#include <Adafruit_GFX.h>
+#include <Tile.h>
+#include <Locator.h>
+#include <Game.h>
+#include <Object.h>
+#include <ImageUtils.h>
+// Commons
+#include <IClockface.h>
+#include <CWDateTime.h>
+
+#include "gfx/assets.h"
+#include "gfx/mario.h"
+#include "gfx/block.h"
+
+namespace cw_cf_0x01 {
+  class Clockface: public IClockface {
+    private:
+      Adafruit_GFX* _display;
+      CWDateTime* _dateTime;
+      void updateTime();
+
+    public:
+      Clockface(Adafruit_GFX* display);
+      void setup(CWDateTime *dateTime);
+      void update();
+      void externalEvent(int type);
+
+  };
+} // namespace cw_cf_0x01
---
+++ firmware/clockfaces/cw-cf-0x02/Clockface.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include <Adafruit_GFX.h>
+#include <Tile.h>
+#include <Locator.h>
+#include <Game.h>
+#include <Object.h>
+#include <ImageUtils.h>
+#include <WiFi.h>
+
+#include "hour8pt7b.h"
+#include "minute7pt7b.h"
+#include "small4pt7b.h"
+#include "DateI18nEN.h"
+
+// Commons
+#include "IClockface.h"
+#include "Icons.h"
+
+namespace cw_cf_0x02 {
+
+class Clockface: public IClockface {
+  private:
+    Adafruit_GFX* _display;
+    CWDateTime* _dateTime;
+    void timeInWords(int h, int m, char* hWords, char* mWords);
+    void updateTime();
+    void updateDate();
+    void updateTemperature();
+
+  public:
+    Clockface(Adafruit_GFX* display);
+    void setup(CWDateTime *dateTime) override;
+    void update() override;
+    void externalEvent(int type) override;
+};
+
+} // namespace cw_cf_0x02
